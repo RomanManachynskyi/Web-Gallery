73d6bc6465f8cb5f48ce93721b6d8395088898c5
RomanManachynskyi <roman.manachynskyi@deepinspire.com>
2024-05-04 22:12:39 +0300

    asw-setup - Created IAM and S3 configs and S3 service

---
+++ WebGallery.Shared.AWS/IAM/IAMConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace WebGallery.Shared.AWS.IAM;
+
+public interface IIamConfig : IAWSConfig
+{
+    string AccessKeyId { get; set; }
+    string SecretAccessKey { get; set; }
+}
+
+public sealed class IamConfig : IIamConfig
+{
+    public string AccessKeyId { get; set; } = string.Empty;
+    public string SecretAccessKey { get; set; } = string.Empty;
+    public string RegionEndpoint { get; set; } = string.Empty;
+}
