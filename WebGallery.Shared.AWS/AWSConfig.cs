524f6ed95032d4c9943392d9476ca834fcfbf181
RomanManachynskyi <roman.manachynskyi@deepinspire.com>
2024-05-04 22:11:53 +0300

    asw-setup - Created AWS and Cognito configs and Cognito service

---
+++ WebGallery.Shared.AWS/AWSConfig.cs
@@ -0,0 +1,45 @@
+﻿using Amazon;
+
+namespace WebGallery.Shared.AWS;
+
+public interface IAWSConfig
+{
+    string RegionEndpoint { get; }
+}
+
+public static class AwsConfig
+{
+    public static RegionEndpoint GetRegionEndpoint(this IAWSConfig config)
+    {
+        if (string.IsNullOrEmpty(config.RegionEndpoint))
+            return RegionEndpoint.EUNorth1; // Stockholm
+
+        return config.RegionEndpoint.ToLower().Trim() switch
+        {
+            "af-south-1" => RegionEndpoint.AFSouth1,
+            "me-south-1" => RegionEndpoint.MESouth1,
+            "ca-central-1" => RegionEndpoint.CACentral1,
+            "cn-north-1" => RegionEndpoint.CNNorth1,
+            "us-gov-west-1" => RegionEndpoint.USGovCloudWest1,
+            "us-gov-east-1" => RegionEndpoint.USGovCloudEast1,
+            "ap-southeast-1" => RegionEndpoint.APSoutheast1,
+            "ap-southeast-2" => RegionEndpoint.APSoutheast2,
+            "ap-south-1" => RegionEndpoint.APSouth1,
+            "ap-northeast-3" => RegionEndpoint.APNortheast3,
+            "sa-east-1" => RegionEndpoint.SAEast1,
+            "ap-northeast-1" => RegionEndpoint.APNortheast1,
+            "ap-northeast-2" => RegionEndpoint.APNortheast2,
+            "us-west-1" => RegionEndpoint.USWest1,
+            "us-west-2" => RegionEndpoint.USWest2,
+            "eu-north-1" => RegionEndpoint.EUNorth1,
+            "eu-west-1" => RegionEndpoint.EUWest1,
+            "us-east-2" => RegionEndpoint.USEast2,
+            "eu-west-3" => RegionEndpoint.EUWest3,
+            "eu-central-1" => RegionEndpoint.EUCentral1,
+            "eu-south-1" => RegionEndpoint.EUSouth1,
+            "ap-east-1" => RegionEndpoint.APEast1,
+            "eu-west-2" => RegionEndpoint.EUWest2,
+            _ => RegionEndpoint.USEast1
+        };
+    }
+}
---
+++ WebGallery.Shared.AWS/Cognito/ICognitoConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace WebGallery.Shared.AWS.Cognito;
+
+public interface ICognitoConfig : IAWSConfig
+{
+    string Authority { get; set; }
+    string UserPoolId { get; set; }
+    string AppClientId { get; set; }
+    string AppClientSecret { get; set; }
+    string ConfirmationLink { get; set; }
+}
+
+public sealed class CognitoConfig : ICognitoConfig
+{
+    public string Authority { get; set; } = string.Empty;
+    public string UserPoolId { get; set; } = string.Empty;
+    public string AppClientId { get; set; } = string.Empty;
+    public string AppClientSecret { get; set; } = string.Empty;
+    public string ConfirmationLink { get; set; } = string.Empty;
+    public string RegionEndpoint { get; set; } = string.Empty;
+}
